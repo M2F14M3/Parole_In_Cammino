c965fdefa4982cfe967b8d57f38132d0fecca72a
Lorenzos001 <solito.lorenzo@protonmail.com>
2022-10-31 18:52:09 +0100

    Update

--- resources/js/page_render.js
+++ resources/js/page_render.js
@@ -6,18 +6,18 @@ const indexes = [
         style: `style="background-color: #e5760d;"`,
         page: "index.html",
         content: `
-            <h1>Chi siamo e cos'è Parole In Cammino?</h1>
+            <h1 id="sub-title">Chi siamo e cos'è Parole In Cammino?</h1>
 
             Buongiorno!<br />
             Benvenuti in questo progetto di tesi.<br />
             Siamo Marta Corti e Francesca Minola, due studentesse del Corso di Laurea di Logopedia, presso l'Università Statale di Milano, sezioni di Bosisio Parini e Fondazione Don Gnocchi.<br />
-            Dal nostro progetto di tesi nasce <span class="key-words">Parole in cammino</span>, un libretto online che vuole essere di agevole lettura e facilmente reperibile.<br />
-            Abbiamo creato un sito all'interno del quale sono illustrate le principali tappe dello<span id="key-word">sviluppo dei bambini tra i 3 e 5 anni.</span> Nello specifico abbiamo approfondito le aree del <span id="key-word">linguaggio</span> e della <span id="key-word">comunicazione</span>, delle <span id="key-word">autonomie personali</span> e delle <span id="key-word">relazioni sociali sviluppate all'interno del gioco</span>.<br />
+            Dal nostro progetto di tesi nasce <span id="sub-title">Parole in cammino</span>, un libretto online che vuole essere di agevole lettura e facilmente reperibile.<br />
+            Abbiamo creato un sito all'interno del quale sono illustrate le principali tappe dello<span id="sub-title">sviluppo dei bambini tra i 3 e 5 anni.</span> Nello specifico abbiamo approfondito le aree del <span id="sub-title">linguaggio</span> e della <span id="sub-title">comunicazione</span>, delle <span id="sub-title">autonomie personali</span> e delle <span id="sub-title">relazioni sociali sviluppate all'interno del gioco</span>.<br />
             Di fondamentale importanza il contributo e la partecipazione del Presidente del CdL Professor Antonio Schindler, della docente del CdL Logopedista Raffaella Pozzoli, delle Logopediste Elena Giudici, Francesca Tombola, Alessandra Brunetti, Silvana Bresciani, dei genitori e delle figure che gravitano attorno ai bambini.<br /><br />
 
-            Parole in cammino è nato in seguito ad uno <span id="key-word">studio osservazionale</span> che ha visto coinvolti genitori, figure professionali e non, che nella loro quotidianità si interfacciano con i bambini.<br />
+            Parole in cammino è nato in seguito ad uno <span id="sub-title">studio osservazionale</span> che ha visto coinvolti genitori, figure professionali e non, che nella loro quotidianità si interfacciano con i bambini.<br />
             A tutti coloro che hanno deciso di partecipare al progetto è stato proposto un questionario online anonimo con lo scopo di indagare le aree di sviluppo approfondite all'interno del libretto.<br />
-            Per questo <span id="key-word">ringraziamo tutti i partecipanti</span> che, rispondendo al questionario, ci hanno permesso di creare uno strumento informativo e di supporto per altri genitori e per tutti coloro che accompagnano i bambini nel loro percorso di crescita.<br /><br />
+            Per questo <span id="sub-title">ringraziamo tutti i partecipanti</span> che, rispondendo al questionario, ci hanno permesso di creare uno strumento informativo e di supporto per altri genitori e per tutti coloro che accompagnano i bambini nel loro percorso di crescita.<br /><br />
 
             Ringraziamo di cuore per il supporto e la preziosa collaborazione:<br />
             Prof. Antonio Schindler, Presidente del Corso di Laurea in Logopedia dell'Università degli Studi di Milano<br />
@@ -36,14 +36,14 @@ const indexes = [
 
             Accompagnare e supportare la crescita di un bambino è uno dei compiti  più complicati e più belli che esistano.<br />
             I bambini crescono molto velocemente e devono imparare tantissime cose fin dai primi giorni di vita; in pochi anni arriveranno a parlare, a camminare, a correre, a osservare, a cercare, a domandare…<br />
-            <span id="key-word">Ogni bambino è unico e non esiste un manuale che contenga delle regole fisse</span> e prestabilite su come debba procedere la sua crescita e su come l'adulto debba accompagnare il suo cammino spesso tortuoso e in salita.<br />
-            Dagli studi condotti fino ad oggi sappiamo che esistono delle <span id="key-word">"tappe</span> che devono essere raggiunte entro un certo periodo di tempo e che sono influenzate in buona parte <span id="key-word">dagli stimoli e dalle persone</span> che circondano il bambino.<br />
+            <span id="sub-title">Ogni bambino è unico e non esiste un manuale che contenga delle regole fisse</span> e prestabilite su come debba procedere la sua crescita e su come l'adulto debba accompagnare il suo cammino spesso tortuoso e in salita.<br />
+            Dagli studi condotti fino ad oggi sappiamo che esistono delle <span id="sub-title">"tappe</span> che devono essere raggiunte entro un certo periodo di tempo e che sono influenzate in buona parte <span id="sub-title">dagli stimoli e dalle persone</span> che circondano il bambino.<br />
             Questi fattori (stimoli e persone) potranno facilitare oppure complicare l'apprendimento di nuove abilità e comportamenti da parte del bambino.<br /><br />
             
-            Il nostro progetto di tesi ha lo scopo di <span id="key-word">diffondere</span> il più possibile le <span id="key-word">conoscenze</span> sulle competenze di un bambino in età prescolare; vorremmo rendere queste preziose informazioni più facilmente accessibili a genitori, pediatri, educatori … a tutti i professionisti che lavorano con questa fascia d'età per favorire un'attenta osservazione dello sviluppo di ciascuno.<br /><br />
+            Il nostro progetto di tesi ha lo scopo di <span id="sub-title">diffondere</span> il più possibile le <span id="sub-title">conoscenze</span> sulle competenze di un bambino in età prescolare; vorremmo rendere queste preziose informazioni più facilmente accessibili a genitori, pediatri, educatori … a tutti i professionisti che lavorano con questa fascia d'età per favorire un'attenta osservazione dello sviluppo di ciascuno.<br /><br />
 
             Speriamo che questo strumento possa essere utile e interessante, che possa incuriosirvi e lasciarvi qualche spunto di riflessione; non abbiamo la presunzione di essere esaustive, perché ogni bambino è un mondo e la sua crescita è un Cammino lungo una vita.<br />
-            <span id="key-word">Vorremmo augurare a tutti gli uomini e le donne di domani un avventuroso viaggio</span>.<br />
+            <span id="sub-title">Vorremmo augurare a tutti gli uomini e le donne di domani un avventuroso viaggio</span>.<br />
             Un soddisfacente Cammino!<br /><br />
             
             <div id="instraction-cointaner">
@@ -54,7 +54,7 @@ const indexes = [
                 ●	Relazioni sociali - gioco.<br />
                 Inoltre abbiamo creato delle "pillole" da lasciarvi come spunti finali: libri, bilinguismo e device.<br /><br />
                 
-                <span id="key-word">In grassetto sono riportate le competenze dei bambini</span>, <span id="key-word2">mentre i consigli e le proposte di attività per l'adulto sono sottolineati.</span>
+                <span id="sub-title">In grassetto sono riportate le competenze dei bambini</span>, <span id="sub-title2">mentre i consigli e le proposte di attività per l'adulto sono sottolineati.</span>
             </div>
         `,
         image: `<img src="resources/images/INTRODUZIONE.png" />`
