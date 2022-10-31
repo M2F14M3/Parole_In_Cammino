fc4185d45316f9e40492311464c46ad99dad1711
Lorenzos001 <solito.lorenzo@protonmail.com>
2022-10-31 16:45:51 +0100

    Update

--- resources/js/page_render.js
+++ resources/js/page_render.js
@@ -8,51 +8,51 @@ const indexes = [
         content: `
             <h1>Chi siamo e cos'è Parole in cammino?</h1>
 
-            Buongiorno!<br/>
-            Benvenuti in questo progetto di tesi.<br/>
-            Siamo Marta Corti e Francesca Minola, due studentesse del Corso di Laurea di Logopedia, presso l'Università Statale di Milano, sezioni di Bosisio Parini e Fondazione Don Gnocchi.<br/>
-            Dal nostro progetto di tesi nasce <span class="key-words">Parole in cammino</span>, un libretto online che vuole essere di agevole lettura e facilmente reperibile.<br/>
-            Abbiamo creato un sito all'interno del quale sono illustrate le principali tappe dello<span id="key-word">sviluppo dei bambini tra i 3 e 5 anni.</span> Nello specifico abbiamo approfondito le aree del <span id="key-word">linguaggio</span> e della <span id="key-word">comunicazione</span>, delle <span id="key-word">autonomie personali</span> e delle <span id="key-word">relazioni sociali sviluppate all'interno del gioco</span>.<br/>
-            Di fondamentale importanza il contributo e la partecipazione del Presidente del CdL Professor Antonio Schindler, della docente del CdL Logopedista Raffaella Pozzoli, delle Logopediste Elena Giudici, Francesca Tombola, Alessandra Brunetti, Silvana Bresciani, dei genitori e delle figure che gravitano attorno ai bambini.<br/><br/>
+            Buongiorno!<br />
+            Benvenuti in questo progetto di tesi.<br />
+            Siamo Marta Corti e Francesca Minola, due studentesse del Corso di Laurea di Logopedia, presso l'Università Statale di Milano, sezioni di Bosisio Parini e Fondazione Don Gnocchi.<br />
+            Dal nostro progetto di tesi nasce <span class="key-words">Parole in cammino</span>, un libretto online che vuole essere di agevole lettura e facilmente reperibile.<br />
+            Abbiamo creato un sito all'interno del quale sono illustrate le principali tappe dello<span id="key-word">sviluppo dei bambini tra i 3 e 5 anni.</span> Nello specifico abbiamo approfondito le aree del <span id="key-word">linguaggio</span> e della <span id="key-word">comunicazione</span>, delle <span id="key-word">autonomie personali</span> e delle <span id="key-word">relazioni sociali sviluppate all'interno del gioco</span>.<br />
+            Di fondamentale importanza il contributo e la partecipazione del Presidente del CdL Professor Antonio Schindler, della docente del CdL Logopedista Raffaella Pozzoli, delle Logopediste Elena Giudici, Francesca Tombola, Alessandra Brunetti, Silvana Bresciani, dei genitori e delle figure che gravitano attorno ai bambini.<br /><br />
 
-            Parole in cammino è nato in seguito ad uno <span id="key-word">studio osservazionale</span> che ha visto coinvolti genitori, figure professionali e non, che nella loro quotidianità si interfacciano con i bambini.<br/>
-            A tutti coloro che hanno deciso di partecipare al progetto è stato proposto un questionario online anonimo con lo scopo di indagare le aree di sviluppo approfondite all'interno del libretto.<br/>
-            Per questo <span id="key-word">ringraziamo tutti i partecipanti</span> che, rispondendo al questionario, ci hanno permesso di creare uno strumento informativo e di supporto per altri genitori e per tutti coloro che accompagnano i bambini nel loro percorso di crescita.<br/><br/>
+            Parole in cammino è nato in seguito ad uno <span id="key-word">studio osservazionale</span> che ha visto coinvolti genitori, figure professionali e non, che nella loro quotidianità si interfacciano con i bambini.<br />
+            A tutti coloro che hanno deciso di partecipare al progetto è stato proposto un questionario online anonimo con lo scopo di indagare le aree di sviluppo approfondite all'interno del libretto.<br />
+            Per questo <span id="key-word">ringraziamo tutti i partecipanti</span> che, rispondendo al questionario, ci hanno permesso di creare uno strumento informativo e di supporto per altri genitori e per tutti coloro che accompagnano i bambini nel loro percorso di crescita.<br /><br />
 
-            Ringraziamo di cuore per il supporto e la preziosa collaborazione:<br/>
-            Prof. Antonio Schindler, Presidente del Corso di Laurea in Logopedia dell'Università degli Studi di Milano<br/>
-            Logopedista Raffaella Pozzoli, Docente del Corso di Laurea in Logopedia dell'Università degli Studi di Milano<br/>
-            Logopedista Elena Giudici<br/>
-            Logopedista Francesca Tombola<br/>
-            Logopedista Alessandra Brunetti<br/>
-            Logopedista Silvana Bresciani<br/>
-            Psicologa Chiara Cantiani, Ricercatrici dell'Istituto La Nostra Famiglia, Bosisio Parini<br/>
-            Psicologa Sara Mascheretti, Ricercatrice del Dipartimento di Scienze del Sistema Nervoso e del Comportamento - Università degli Studi di Pavia<br/>
-            Lorenzo Solito, sviluppatore del sito<br/>
-            Lara Zerbi, realizzatrice delle illustrazioni di "Parole in Cammino"<br/>
-            Walter Crippa, graphic designer<br/>
+            Ringraziamo di cuore per il supporto e la preziosa collaborazione:<br />
+            Prof. Antonio Schindler, Presidente del Corso di Laurea in Logopedia dell'Università degli Studi di Milano<br />
+            Logopedista Raffaella Pozzoli, Docente del Corso di Laurea in Logopedia dell'Università degli Studi di Milano<br />
+            Logopedista Elena Giudici<br />
+            Logopedista Francesca Tombola<br />
+            Logopedista Alessandra Brunetti<br />
+            Logopedista Silvana Bresciani<br />
+            Psicologa Chiara Cantiani, Ricercatrici dell'Istituto La Nostra Famiglia, Bosisio Parini<br />
+            Psicologa Sara Mascheretti, Ricercatrice del Dipartimento di Scienze del Sistema Nervoso e del Comportamento - Università degli Studi di Pavia<br />
+            Lorenzo Solito, sviluppatore del sito<br />
+            Lara Zerbi, realizzatrice delle illustrazioni di "Parole in Cammino"<br />
+            Walter Crippa, graphic designer<br />
 
             <h1>Lettera ai genitori e alle figure di riferimento</h1>
 
-            Accompagnare e supportare la crescita di un bambino è uno dei compiti  più complicati e più belli che esistano.<br/>
-            I bambini crescono molto velocemente e devono imparare tantissime cose fin dai primi giorni di vita; in pochi anni arriveranno a parlare, a camminare, a correre, a osservare, a cercare, a domandare…<br/>
-            <span id="key-word">Ogni bambino è unico e non esiste un manuale che contenga delle regole fisse</span> e prestabilite su come debba procedere la sua crescita e su come l'adulto debba accompagnare il suo cammino spesso tortuoso e in salita.<br/>
-            Dagli studi condotti fino ad oggi sappiamo che esistono delle <span id="key-word">"tappe</span> che devono essere raggiunte entro un certo periodo di tempo e che sono influenzate in buona parte <span id="key-word">dagli stimoli e dalle persone</span> che circondano il bambino.<br/>
-            Questi fattori (stimoli e persone) potranno facilitare oppure complicare l'apprendimento di nuove abilità e comportamenti da parte del bambino.<br/><br/>
+            Accompagnare e supportare la crescita di un bambino è uno dei compiti  più complicati e più belli che esistano.<br />
+            I bambini crescono molto velocemente e devono imparare tantissime cose fin dai primi giorni di vita; in pochi anni arriveranno a parlare, a camminare, a correre, a osservare, a cercare, a domandare…<br />
+            <span id="key-word">Ogni bambino è unico e non esiste un manuale che contenga delle regole fisse</span> e prestabilite su come debba procedere la sua crescita e su come l'adulto debba accompagnare il suo cammino spesso tortuoso e in salita.<br />
+            Dagli studi condotti fino ad oggi sappiamo che esistono delle <span id="key-word">"tappe</span> che devono essere raggiunte entro un certo periodo di tempo e che sono influenzate in buona parte <span id="key-word">dagli stimoli e dalle persone</span> che circondano il bambino.<br />
+            Questi fattori (stimoli e persone) potranno facilitare oppure complicare l'apprendimento di nuove abilità e comportamenti da parte del bambino.<br /><br />
             
-            Il nostro progetto di tesi ha lo scopo di <span id="key-word">diffondere</span> il più possibile le <span id="key-word">conoscenze</span> sulle competenze di un bambino in età prescolare; vorremmo rendere queste preziose informazioni più facilmente accessibili a genitori, pediatri, educatori … a tutti i professionisti che lavorano con questa fascia d'età per favorire un'attenta osservazione dello sviluppo di ciascuno.<br/><br/>
+            Il nostro progetto di tesi ha lo scopo di <span id="key-word">diffondere</span> il più possibile le <span id="key-word">conoscenze</span> sulle competenze di un bambino in età prescolare; vorremmo rendere queste preziose informazioni più facilmente accessibili a genitori, pediatri, educatori … a tutti i professionisti che lavorano con questa fascia d'età per favorire un'attenta osservazione dello sviluppo di ciascuno.<br /><br />
 
-            Speriamo che questo strumento possa essere utile e interessante, che possa incuriosirvi e lasciarvi qualche spunto di riflessione; non abbiamo la presunzione di essere esaustive, perché ogni bambino è un mondo e la sua crescita è un Cammino lungo una vita.<br/>
-            <span id="key-word">Vorremmo augurare a tutti gli uomini e le donne di domani un avventuroso viaggio</span>.<br/>
-            Un soddisfacente Cammino!<br/><br/>
+            Speriamo che questo strumento possa essere utile e interessante, che possa incuriosirvi e lasciarvi qualche spunto di riflessione; non abbiamo la presunzione di essere esaustive, perché ogni bambino è un mondo e la sua crescita è un Cammino lungo una vita.<br />
+            <span id="key-word">Vorremmo augurare a tutti gli uomini e le donne di domani un avventuroso viaggio</span>.<br />
+            Un soddisfacente Cammino!<br /><br />
             
             <div id="instraction-cointaner">
-                <span id="sub-title">COME E' STRUTTURATO IL SITO</span><br/>
-                Il sito è diviso in capitoli per fasce d'età e in ogni sezione troverete i diversi argomenti nei rispettivi box:<br/>
-                ●	Linguaggio<br/>
-                ●	Autonomie<br/>
-                ●	Relazioni sociali - gioco<br/>
-                Inoltre abbiamo creato delle "pillole" da lasciarvi come spunti finali: libri, bilinguismo e device.<br/><br/>
+                <span id="sub-title">COME E' STRUTTURATO IL SITO</span><br />
+                Il sito è diviso in capitoli per fasce d'età e in ogni sezione troverete i diversi argomenti nei rispettivi box:<br />
+                ●	Linguaggio.<br />
+                ●	Autonomie.<br />
+                ●	Relazioni sociali - gioco.<br />
+                Inoltre abbiamo creato delle "pillole" da lasciarvi come spunti finali: libri, bilinguismo e device.<br /><br />
                 
                 <span id="key-word">In grassetto sono riportate le competenze dei bambini</span>, <span id="key-word2">mentre i consigli e le proposte di attività per l'adulto sono sottolineati.</span>
             </div>
@@ -65,7 +65,7 @@ const indexes = [
         indexValue: "3 anni",
         style: `style="background-color: #8e1582;"`,
         page: "three_years_old.html",
-        content: "",
+        content: ``,
         image: ``
     },
     {
@@ -74,7 +74,7 @@ const indexes = [
         indexValue: "3-4 anni",
         style: `style="background-color: #e5760d;"`,
         page: "three_four_years_old.html",
-        content: "",
+        content: ``,
         image: `<img src="resources/images/" />`
     },
     {
@@ -83,7 +83,7 @@ const indexes = [
         indexValue: "4-5 anni",
         style: `style="background-color: #44b513;"`,
         page: "four_five_years_old.html",
-        content: "",
+        content: ``,
         image: `<img src="resources/images/" />`
     },
     {
@@ -92,7 +92,7 @@ const indexes = [
         indexValue: "5-6 anni",
         style: `style="background-color: #3b5bed;"`,
         page: "five_six_years_old.html",
-        content: "",
+        content: ``,
         image: `<img src="resources/images/" />`
     },
     {
@@ -101,7 +101,7 @@ const indexes = [
         indexValue: "Lettura",
         style: `style="background-color: #8e1582;"`,
         page: "reading.html",
-        content: "",
+        content: ``,
         image: `<img src="resources/images/" />`
     },
     {
@@ -110,7 +110,7 @@ const indexes = [
         indexValue: "Bilinguismo",
         style: `style="background-color: #cc0000;"`,
         page: "bilinguism.html",
-        content: "",
+        content: ``,
         image: `<img src="resources/images/" />`
     },
     {
@@ -119,8 +119,43 @@ const indexes = [
         indexValue: "Device",
         style: `style="background-color: #354f0b;"`,
         page: "device.html",
-        content: "",
-        image: `<img src="resources/images/" />`
+        content: `
+            <h1 id="sub-title">TECNOLOGIA</h1>
+
+            Gli strumenti tecnologici sono ormai entrati a far parte delle nostre vite, ma è importante prestare attenzione al modo in cui si usano.<br />
+            Nel 2018 i pediatri italiani hanno preso posizione sull'<span id="sub-title">uso dei dispositivi tecnologici in età prescolare:</span><br />
+            
+            <div id="instraction-cointaner">
+                ●	consigliano di non dare al bambino strumenti digitali prima dei 2 anni.<br />
+                ●	dai 2 ai 5 anni lo strumento digitale non deve essere usato per più di un'ora al giorno ed è sempre necessaria la supervisione di un adulto.<br />
+                ●	dai 5 agli 8 anni il tempo di fruizione può al massimo raddoppiare (con accanto il genitore).<br />
+                ●	mai utilizzarlo durante i pasti o prima di andare a dormire e mai sottoporre contenuti violenti.<br />
+                ●	la tecnologia non deve avere lo scopo di calmare o distrarre i più piccoli perché limita lo sviluppo della regolazione delle emozioni nei bambini.<br />
+            </div><br />
+            
+            Ecco alcuni consigli da poter attuare quando si parla di strumenti tecnologici:<br />
+            -	è importante <span id="sub-title">limitare l'uso prolungato</span> degli schermi.<br />
+            -	nei brevi momenti in cui questi strumenti vengono utilizzati è utile <span id="sub-title">dare il buon esempio</span> su un uso consapevole.<br />
+            -	è importante <span id="sub-title">assicurarsi che app, giochi e programmi siano davvero adatti</span> all'età dei bambini: per esempio sarebbe meglio evitare giochi con i combattimenti piuttosto che applicazioni in cui ci sono azioni illegali o diseducative (rapine in banca, graffiti sui muri, fughe dalla polizia …).<br /><br />
+            
+            <span id="sub-title2">Quali rischi potrebbero derivare da un eccessivo utilizzo?</span><br />
+            Se il tempo dedicato all'uso della tecnologia è elevato nella vita di tutti i giorni la posizione rigida tenuta dai bambini potrebbe essere causa di <span id="sub-title">dolori muscolari</span> alle spalle e al collo.<br />
+            La sedentarietà legata ai device potrebbe inoltre causare un <span id="sub-title">aumento di peso e un'irregolarità del sonno</span>: i bambini dormono meno ore per notte e si addormentano con maggiore fatica.<br />
+            Un ultimo problema che è opportuno segnalare riguarda le stimolazioni eccessive: la luminosità degli schermi e i suoni troppo elevati possono <span id="sub-title">danneggiare la vista e l'udito</span>, che sono ancora in via di sviluppo durante questa età.<br />
+            Per non parlare della <span id="sub-title">facile dipendenza</span> che può essere provocata dalla prolungata esposizione alla tecnologia, con conseguenti comportamenti insistenti e capricciosi per ottenere quegli schermi.<br /><br />
+            
+            Quindi…utilizzati con i <span id="sub-title">tempi e le modalità adeguate</span> all'età dei bambini, i dispositivi elettronici possono offrire l'opportunità di imparare in modo giocoso e divertente, MA è <span id="sub-title">indispensabile che l'adulto affianchi il bambino</span> per commentare e portare nella quotidianità gli stimoli ascoltati: un bambino che utilizza in solitaria la tecnologia (subendo in modo passivo lo strumento) NON sarà in grado di apprendere!<br /><br />
+            
+            Qualche <span id="sub-title2">consiglio pratico per evitare la dipendenza dagli schermi</span>:<br />
+            ★	quando prendiamo una scelta o imponiamo delle regole (soprattutto se si tratta di schermi), <span id="sub-title">spieghiamo al bambino il perché</span>. Cominceremo a renderli consapevoli.<br />
+            ★	in momenti di attesa fuori casa (al ristorante, dal dottore, dal dentista …) portiamo dei giochi specifici: uno zainetto con <span id="sub-title">attività che fanno raramente</span> (dei colori particolari, delle carte nuove …) e solo in quell’occasione di attesa. La “novità” potrebbe allontanarli facilmente dal telefonino.<br />
+            ★	se attraverso i device emerge una passione del bambino (scopre i dinosauri grazie ai documentari, o gli piace il personaggio di un cartone, o adora un cantante …), <span id="sub-title">ampliamo questo argomento anche fuori dalla tecnologia</span>: cerchiamo dei libri inerenti, o dei personaggini giocattolo, piuttosto che gli stickers …<br />
+            Così da enfatizzare e dare importanza alla sua passione e non al momento con il tablet.<br />
+            ★	si può anche cercare di <span id="sub-title">non enfatizzare l'uso della tecnologia</span>: evitiamo di usarla come premio o di toglierla in caso di capricci (sembrerà che il dispositivo sia qualcosa di bello e desiderabile).<br /><br />
+            
+            BIBLIOGRAFIA: (Lavenia, 2019); (Chaibal S e Chaiyakul S., 2022); (Moon et al., 2019); (Yadav e Chakraborty, 2018); (Bozzola et al., 2018). 
+        `,
+        image: `<img src="resources/images/DEVICE.jpg" />`
     },
     {
         id: 8,
@@ -128,7 +163,7 @@ const indexes = [
         indexValue: "Dove altro guardare",
         style: `style="background-color: #e5760d;"`,
         page: "bibliography.html",
-        content: "",
+        content: ``,
         image: `<img src="resources/images/" />`
     }
 ]
@@ -187,6 +222,9 @@ function contentRender() {
         case "Device - Parole In Cammino":
             deviceContent();
             break;
+        case "Dove altro guardare - Parole In Cammino":
+            bibliography();
+            break;
     }
 }
 
@@ -230,5 +268,20 @@ function bilinguismContent() {
 }
 
 function deviceContent() {
+    const data = document.querySelector("#data");
+    const content = document.createElement("div");
+    content.id = "content";
+    content.innerHTML = `
+        <div id="p1">
+            ${indexes[7].content}
+        </div>
+        <div id="image">
+            ${indexes[7].image}
+        </div>
+    `;
+    data.appendChild(content);
+}
+
+function bibliography() {
 
 }
